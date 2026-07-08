b0029c9aa775ba696c1c6e5c73fdf3a3da031900
rakshya12_233 <rakshyabastola32@gmail.com>
2026-07-09 00:48:34 +0545

    latest changes for deployment

--- main.py
+++ main.py
@@ -12,70 +12,13 @@ from slowapi.errors import RateLimitExceeded
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
-cache_data=[]
-limiter=Limiter(key_func=get_remote_address)
-app.state.limiter=limiter
 
-last_fetch=0
-url="https://example.com"
-response=requests.get(url)
-soup=BeautifulSoup(response.text,"html.parser")
-print(soup.title.text)
-app.add_middleware(CORSMiddleware,allow_origins=settings.origins,allow_credentials=True,allow_methods=["*"],allow_headers=["*"]) 
-@app.exception_handler(RateLimitExceeded)
-def rate_limit_handler(request:Request, exc:RateLimitExceeded):
-    return JSONResponse(content={"error": "Rate limit exceeded"}, status_code=429)
-
-@app.get("/data")
-@limiter.limit("5/minute")
-def getdata(request:Request):
-    return {"message":"Success"}
-@app.get("/news")
-def get_news(page:int=1,limit:int=10):
-    global cache_data,last_fetch
-    start=time.time()
-    if time.time()-last_fetch>60:
-        print("fetching fresh data")
-        url="https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen"
-        response=requests.get(url)
-        soup=BeautifulSoup(response.text,"html.parser")
-        cache_data=[
-            item.text for item in soup.find_all("a",class_="svxzne")
-        ]
-        
-        last_fetch=time.time()
-
-    else:
-        print("fetching from cache")
-    end=time.time()
-    time_take=round(end-start,4)
-    print("time taken",time_take)
-    return {
-        "time_taken":time_take,
-        "page":page,
-        "limit":limit,
-        "total_news":len(cache_data),
-        "news":cache_data[(page-1)*limit:page*limit]
-    }
+   
   
       
        
 
-@app.get("/posts")
-def get_posts(page:int=1,limit:int=10):
-    url="https://jsonplaceholder.typicode.com/posts/"
-    start=(page-1)*limit
-    end=start+limit
-    responses=requests.get(url)
-    return{"page":page,"limit":limit,"total_posts":len(responses.json()),"posts":responses.json()[start:end]}
-@app.get("/posts/{id}")
-def get_posts(id:int):
-    url=f"https://jsonplaceholder.typicode.com/posts/{id}"
-    response=requests.get(url)
-    if response.status_code==200:
-        return response.json()
-    else:
-        raise HTTPException(status_code=404,detail="Post not found")
+
 #Step -1  Ensure upload folderexist
 UPLOAD_DIR = "uploads"
 if not os.path.exists(UPLOAD_DIR):
--- secondmain.py
+++ secondmain.py
@@ -1,163 +1,163 @@
-from fastapi import FastAPI,status,HTTPException,Depends
-from sqlalchemy import create_engine,Column,Integer,String
-from sqlalchemy.orm import sessionmaker,declarative_base,Session
-from jose import jwt
-from datetime import datetime,timedelta,timezone
-import time
-import os
-from config import settings
-import asyncio
-from dotenv import load_dotenv
-from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
-from passlib.context import CryptContext
-app=FastAPI()
+# from fastapi import FastAPI,status,HTTPException,Depends
+# from sqlalchemy import create_engine,Column,Integer,String
+# from sqlalchemy.orm import sessionmaker,declarative_base,Session
+# from jose import jwt
+# from datetime import datetime,timedelta,timezone
+# import time
+# import os
+# from config import settings
+# import asyncio
+# from dotenv import load_dotenv
+# from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+# from passlib.context import CryptContext
+# app=FastAPI()
 
 
-ALGORITHM="HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES=30
+# ALGORITHM="HS256"
+# ACCESS_TOKEN_EXPIRE_MINUTES=30
  
-#PASSWORD hashing setup
-pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
-#Oauth setup
-oauth2_schema=OAuth2PasswordBearer(tokenUrl="login")
-#dummy user database
-fake_user_db={
-    "admin":{
-        "username":"admin",
-        "hashed_password":pwd_context.hash("1234")
-    }
+# #PASSWORD hashing setup
+# pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
+# #Oauth setup
+# oauth2_schema=OAuth2PasswordBearer(tokenUrl="login")
+# #dummy user database
+# fake_user_db={
+#     "admin":{
+#         "username":"admin",
+#         "hashed_password":pwd_context.hash("1234")
+#     }
     
-}
-#hash password
-def hash_password(password:str):
-    return pwd_context.hash(password)
-#verify password
-def verify_password(plain_password,hashed_password):
-    return pwd_context.verify(plain_password,hashed_password)
-#create token
-def create_token(data:dict):
-    to_encode=data.copy()
-    expire=datetime.now(timezone.utc)+timedelta(minutes=30)
-    to_encode.update({"exp":expire})
-    token=jwt.encode(to_encode,settings.SECRET_KEY,algorithm=ALGORITHM)
-    return token
-#token verify
-def tokenVerify(token:str=Depends(oauth2_schema)):
-    try:
-        payload=jwt.decode(token,settings.SECRET_KEY,algorithms=[ALGORITHM])
-        username:str=payload.get("sub")
-        if username is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid token")
+# }
+# #hash password
+# def hash_password(password:str):
+#     return pwd_context.hash(password)
+# #verify password
+# def verify_password(plain_password,hashed_password):
+#     return pwd_context.verify(plain_password,hashed_password)
+# #create token
+# def create_token(data:dict):
+#     to_encode=data.copy()
+#     expire=datetime.now(timezone.utc)+timedelta(minutes=30)
+#     to_encode.update({"exp":expire})
+#     token=jwt.encode(to_encode,settings.SECRET_KEY,algorithm=ALGORITHM)
+#     return token
+# #token verify
+# def tokenVerify(token:str=Depends(oauth2_schema)):
+#     try:
+#         payload=jwt.decode(token,settings.SECRET_KEY,algorithms=[ALGORITHM])
+#         username:str=payload.get("sub")
+#         if username is None:
+#             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid token")
         
-        return username
-    except jwt.JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid token")
+#         return username
+#     except jwt.JWTError:
+#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid token")
         
-#loginapi(generate token
-@app.post("/login")
-def login(form_data:OAuth2PasswordRequestForm=Depends()):
-    user=fake_user_db.get(form_data.username)
-    if not user or not verify_password(form_data.password,user["hashed_password"]):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Invalid username or password")
-    access_token=create_token({
-        "sub":form_data.username
-    })
-    return {"access_token":access_token,"token_type":"bearer"}
+# #loginapi(generate token
+# @app.post("/login")
+# def login(form_data:OAuth2PasswordRequestForm=Depends()):
+#     user=fake_user_db.get(form_data.username)
+#     if not user or not verify_password(form_data.password,user["hashed_password"]):
+#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Invalid username or password")
+#     access_token=create_token({
+#         "sub":form_data.username
+#     })
+#     return {"access_token":access_token,"token_type":"bearer"}
         
-@app.get("/secure-data")
-def secure_data(user=Depends(tokenVerify)):
-    return {
-        "message":"secure data accessed",
-        "user":user
-    }
-@app.get("/protected")
-def protected_route(username:str=Depends(tokenVerify)):
-    return {
-        "message":f"hello ${username} you have access to this protected route",
-        "user":username
+# @app.get("/secure-data")
+# def secure_data(user=Depends(tokenVerify)):
+#     return {
+#         "message":"secure data accessed",
+#         "user":user
+#     }
+# @app.get("/protected")
+# def protected_route(username:str=Depends(tokenVerify)):
+#     return {
+#         "message":f"hello ${username} you have access to this protected route",
+#         "user":username
      
-    }
+#     }
 
 
-engine=create_engine(settings.DATABASE_URL,connect_args={"check_same_thread":False})
-sessionLocal=sessionmaker(bind=engine)
-Base=declarative_base()
-def task():
-   time.sleep(3)
-   return "Done"
-async def task():
-    await asyncio.sleep(3)
-    return "Done"
-class Todo(Base):
-    __tablename__="todo"
-    id=Column(Integer,primary_key=True,index=True)
-    title=Column(String)
+# engine=create_engine(settings.DATABASE_URL,connect_args={"check_same_thread":False})
+# sessionLocal=sessionmaker(bind=engine)
+# Base=declarative_base()
+# def task():
+#    time.sleep(3)
+#    return "Done"
+# async def task():
+#     await asyncio.sleep(3)
+#     return "Done"
+# class Todo(Base):
+#     __tablename__="todo"
+#     id=Column(Integer,primary_key=True,index=True)
+#     title=Column(String)
   
-    completed=Column(Integer)
+#     completed=Column(Integer)
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 
-def get_db():
-    db=sessionLocal()
-    try:
-        yield db
-    finally:
-        db.close
+# def get_db():
+#     db=sessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close
     
-@app.get("/")
-async def home(db:Session=Depends(get_db)):
-    await asyncio.sleep(3)
-    return {"message":"db connected successfully"}
+# @app.get("/")
+# async def home(db:Session=Depends(get_db)):
+#     await asyncio.sleep(3)
+#     return {"message":"db connected successfully"}
         
 
-@app.post("/todos")
-def createTodo(title:str,completed:bool,db:Session=Depends(get_db)):
-    todo=Todo(title=title,completed=completed)
+# @app.post("/todos")
+# def createTodo(title:str,completed:bool,db:Session=Depends(get_db)):
+#     todo=Todo(title=title,completed=completed)
     
-    db.add(todo)
-    db.commit()
-    db.refresh(todo)
-    return {
-        "message":"todo created successfully",
-        "data":todo
-    }
+#     db.add(todo)
+#     db.commit()
+#     db.refresh(todo)
+#     return {
+#         "message":"todo created successfully",
+#         "data":todo
+#     }
 
-@app.get("/todos")
-def showTodo(db:Session=Depends(get_db)):
-    todos=db.query(Todo).all()
-    return{
-        "total":len(todos),
-        "data":todos
-    }
+# @app.get("/todos")
+# def showTodo(db:Session=Depends(get_db)):
+#     todos=db.query(Todo).all()
+#     return{
+#         "total":len(todos),
+#         "data":todos
+#     }
     
-@app.get("/todos/{todo_id}")
-def showTodoById(id=int,db:Session=Depends(get_db)):
-    todos=db.query(Todo).filter(Todo.id==id).first()
-    if not  todos:
-        raise HTTPException(status_code=404,detail="todo not found")
-    return todos
+# @app.get("/todos/{todo_id}")
+# def showTodoById(id=int,db:Session=Depends(get_db)):
+#     todos=db.query(Todo).filter(Todo.id==id).first()
+#     if not  todos:
+#         raise HTTPException(status_code=404,detail="todo not found")
+#     return todos
 
-@app.put("/todos/{todo_id}")
-def updateTodoById(id=int,title=str,db:Session=Depends(get_db)):
-    todos=db.query(Todo).filter(Todo.id==id).first()
-    if not todos:
-        raise HTTPException(status_code=404,detail="todo not found")
-    todos.title=title
-    db.commit()
-    db.refresh(todos)
-    return {
-        "message":"todo updated successfully",
-        "data":todos
-    }
+# @app.put("/todos/{todo_id}")
+# def updateTodoById(id=int,title=str,db:Session=Depends(get_db)):
+#     todos=db.query(Todo).filter(Todo.id==id).first()
+#     if not todos:
+#         raise HTTPException(status_code=404,detail="todo not found")
+#     todos.title=title
+#     db.commit()
+#     db.refresh(todos)
+#     return {
+#         "message":"todo updated successfully",
+#         "data":todos
+#     }
     
-@app.delete("/todos/{todo_id}")
-def deleteById(id=int,db:Session=Depends(get_db)):
-    todos=db.query(Todo).filter(Todo.id==id).first()
-    if not todos:
-        raise HTTPException(status_code=404,detail="todo not found")
-    db.delete(todos)
-    db.commit()
-    return {
-        "message":"todo deleted successfully"
-    }
+# @app.delete("/todos/{todo_id}")
+# def deleteById(id=int,db:Session=Depends(get_db)):
+#     todos=db.query(Todo).filter(Todo.id==id).first()
+#     if not todos:
+#         raise HTTPException(status_code=404,detail="todo not found")
+#     db.delete(todos)
+#     db.commit()
+#     return {
+#         "message":"todo deleted successfully"
+#     }
